616f308a0fafb042e24b069ec632b9a3de566c83
Feni <fenisonii2004@gmail.com>
2025-03-10 12:16:18 +0530

    gfr

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -24,21 +24,21 @@ export default function Navbar(){
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-ld md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          < Link to="/Home" class="block py-2 px-3 text-white text-2xl hover:text-purple-700rounded-sm md:bg-transparent
+          < Link to="/Home" class="block py-2 px-3 text-white text-xl hover:text-purple-700rounded-sm md:bg-transparent
            md:text-white-700 md:p-0  md:dark:text-white-500  md:hover:text-purple-700"
             aria-current="page">
               <FontAwesomeIcon icon={faHouse}></FontAwesomeIcon> Home</Link>
         </li>
         <li>
             <Link to="/about" class="block py-2 px-3 text-white rounded-sm
-           hover:bg-gray-100 md:hover:bg-transparent md:border-0 text-2xl
+           hover:bg-gray-100 md:hover:bg-transparent md:border-0 text-xl
             md:hover:text-purple-700 md:p-0 dark:text-white md:dark:hover:text-blue-500
              dark:hover:bg-gray-700 dark:hover:text-white
               md:dark:hover:bg-transparent">
                 <FontAwesomeIcon icon={faCircleUser}></FontAwesomeIcon> About</Link>
         </li>
         <li>
-          <Link to= "/projects" class="block py-2 px-3 text-white rounded-sm text-2xl
+          <Link to= "/projects" class="block py-2 px-3 text-white rounded-sm text-xl
            hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-purple-700 
            md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700
             dark:hover:text-white md:dark:hover:bg-transparent">
@@ -47,7 +47,7 @@ export default function Navbar(){
         </li>
        
        <li>
-       <Link to= "/projectData" class="block py-2 px-3 text-white rounded-sm text-2xl
+       <Link to= "/projectData" class="block py-2 px-3 text-white rounded-sm text-xl
            hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-purple-700 
            md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700
             dark:hover:text-white md:dark:hover:bg-transparent">
